d436705809f8ae9d5121d6a9c28ea1a5964d808b
Dennis <112337016+TheProgrammer1000@users.noreply.github.com>
2023-11-01 18:27:47 +0100

    secound commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="theprogrammer",
+  password="Kalleanka9!",
+  db="secureProject"
+)
+
+print(mydb)
+
 print("TJABA")
